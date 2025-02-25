2d967f1dbb204b8517a870a95b89f9c65a695ce5
calvincirwin <13965288+calvincirwin@users.noreply.github.com>
2025-02-24 20:14:08 -0700

    Fixed TypeScript error by ensuring PORT is a number

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,7 @@ import weatherRoutes from './routes/api/weatherRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = Number(process.env.PORT) || 3001;
 
 // ✅ Allow API to be accessed from anywhere
 app.use(cors());
